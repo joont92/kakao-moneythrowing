6f71883c39781500e5dcc8ac3b02eb1e618ee69c
joont92 <pk920207@gmail.com>
2020-08-29 17:33:23 +0900

    init project

---
+++ src/main/java/com/kakao/moneythrowing/MoneythrowingApplication.java
@@ -0,0 +1,13 @@
+package com.kakao.moneythrowing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneythrowingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneythrowingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kakao/moneythrowing/MoneythrowingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakao.moneythrowing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneythrowingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
